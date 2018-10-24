942f6d8c9cabe0ad6c3744cb90a393e5359d8362
nalika <thangara@uwindsor.ca>
2018-10-24 15:06:47 -0400

    Create BalancedParen.py

---
+++ BalancedParen.py
@@ -0,0 +1,23 @@
+def BalancedParen(str):
+
+	bSet = {'(':')', '[':']', '{':'}'} # balanced set
+	# print(bSet)
+
+	# scan str
+	pSet = []
+	for c in str:
+		# print(bSet[c])
+		if c == '(' or c =='[' or c == '{': # open paren must before closing
+			pSet.append(c)
+		elif c == ')' or c ==']' or c == '}':
+			if not pSet: # if there was no opening then it is imbalanced 
+				print('imbalanced list: ' + str)
+				return False
+			elif bSet[pSet[-1]] == c:
+				pSet.remove(pSet[-1])				
+			else:
+				print('imbalanced list: ' + str)
+				return False
+	if not pSet:
+		print('balanced list: ' + str)
+		return True
